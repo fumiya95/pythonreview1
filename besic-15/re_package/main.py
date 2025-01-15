cadf31b669014192beb40a469cf0495b8acb9886
fumiya <156275318+fumiya95@users.noreply.github.com>
2025-01-15 11:30:26 +0900

    Add files via upload

--- besic-15/re_package/main.py
+++ besic-15/re_package/main.py
@@ -1,6 +1,9 @@
 from sports_package import baseball, boxing, swimming
+from datetime import datetime
 
 print(baseball.hit())
 print(boxing.jab())
 print(swimming.butterfly())
 print(boxing.style())
+
+print(datetime.now())
